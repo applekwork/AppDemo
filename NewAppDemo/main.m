71d2ba89f9f0f63b3cfc0c690d453529340e8453
郭立军 <guolijun@zuhaowandeMac.local>
2018-03-23 17:03:18 +0800

    Initial Commit

---
+++ NewAppDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewAppDemo
+//
+//  Created by 郭立军 on 2018/3/23.
+//  Copyright © 2018年 com.guolijun.daofeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
